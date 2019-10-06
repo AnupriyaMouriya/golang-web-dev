cdaa0bc012b5b9babccbc152e3c16e3574e3fb6d
AnupriyaMouriya <43275899+AnupriyaMouriya@users.noreply.github.com>
2019-10-07 00:31:16 +0530

    Update main.go

--- 044_postgres/16_go-postgres/main.go
+++ 044_postgres/16_go-postgres/main.go
@@ -7,13 +7,14 @@ import (
 )
 
 func main() {
+	//func Open(driverName , dataSourceName string)(*DB , error)
 	db, err := sql.Open("postgres", "postgres://bond:password@localhost/bookstore?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	err = db.Ping()
+	err = db.Ping() //it checks if the connection is well established or not else throws an error
 	if err != nil {
 		panic(err)
 	}
